1abf1d83d461e97f485af0a7a0773205ca4ab486
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2025-02-14 09:15:22 -0300

    blink led rgb

--- intruder_alarm.c
+++ intruder_alarm.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+#define LED_PIN 12
 
 int main()
 {
     stdio_init_all();
 
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
     while (true) {
-        printf("Hello, world!\n");
+        gpio_put(LED_PIN, 1);
+        sleep_ms(1000);
+        gpio_put(LED_PIN, 0);
         sleep_ms(1000);
     }
 }
