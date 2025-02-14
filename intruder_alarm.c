efce86ff66fef7ed2bb0c4c47e024b191be15262
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2025-02-14 09:29:23 -0300

    read microphone

--- intruder_alarm.c
+++ intruder_alarm.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
+/* definindo pinos do led e microfone */
 #define LED_PIN 12
+#define MIC_CHANNEL 2
+#define MIC_PIN 28
+
 
 int main()
 {
     stdio_init_all();
 
+    /* iniciando e configurando pinos */
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    adc_gpio_init(MIC_PIN);
+    adc_init();
+    adc_select_input(MIC_CHANNEL);
+
+
     while (true) {
+        uint16_t noise = adc_read();
+        printf("%u\n", noise);
         gpio_put(LED_PIN, 1);
         sleep_ms(1000);
         gpio_put(LED_PIN, 0);
