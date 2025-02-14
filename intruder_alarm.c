1d451744bab055ed6d84cd9a243f022e927e650a
Vitor-Silva27 <developer.vitorsilva@gmail.com>
2025-02-14 09:55:57 -0300

    cleaning up code

--- intruder_alarm.c
+++ intruder_alarm.c
@@ -8,25 +8,14 @@
 #define MIC_PIN 28
 
 #define SAMPLES 200
+#define MIN_NOISE 2500
 
 uint16_t adc_buffer[SAMPLES];
 bool led_state = false;
 
-void sample_mic() {
-    for (uint i = 0; i < SAMPLES; i++) {
-        adc_buffer[i] = adc_read();
-    }
-}
-
-uint16_t get_peak() {
-    uint16_t max_value = 0;
-    for (uint i = 0; i < SAMPLES; i++) {
-        if (adc_buffer[i] > max_value) {
-            max_value = adc_buffer[i];
-        }
-    }
-    return max_value;
-}
+/* prototipos de funcção */
+void sample_mic();
+uint16_t get_peak();
 
 int main()
 {
@@ -45,7 +34,9 @@ int main()
         sample_mic();
         int16_t peak = get_peak();
         
-        if (peak > 2500) {
+        /* verificando se o som é mais alto que o valor minimo que defini */
+        if (peak > MIN_NOISE) {
+            /* alternando o valor do estado do led, para mantelo apagado ou aceso */
             led_state = !led_state;
             gpio_put(LED_PIN, led_state);
             sleep_ms(500);
@@ -53,4 +44,23 @@ int main()
         
         printf("%u\n", peak);
     }
+}
+
+
+/* lendo o microfone a diconando em um buffer, para ter uma leitura mais precisa */
+void sample_mic() {
+    for (uint i = 0; i < SAMPLES; i++) {
+        adc_buffer[i] = adc_read();
+    }
+}
+
+/* pegando o som mais alto */
+uint16_t get_peak() {
+    uint16_t max_value = 0;
+    for (uint i = 0; i < SAMPLES; i++) {
+        if (adc_buffer[i] > max_value) {
+            max_value = adc_buffer[i];
+        }
+    }
+    return max_value;
 }
